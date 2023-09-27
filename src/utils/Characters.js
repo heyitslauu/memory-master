f69d9b4c5722f0182e76182ae4480e07af663e44
heyitslauuu <laurencecadag1@gmail.com>
2023-09-27 11:00:47 +0800

    Character Utils

---
+++ src/utils/Characters.js
@@ -0,0 +1,70 @@
+import { v4 as uuidv4 } from 'uuid';
+
+// import beth from "../assets/beth.png";
+import birdperson from "../assets/birdperson.png";
+import jerry from "../assets/jerry.png";
+import mee6 from "../assets/mee6.png";
+import morty from "../assets/morty.png";
+import picklerick from "../assets/picklerick.png";
+import rick from "../assets/rick.png";
+import snuffles from "../assets/snuffles.png";
+import summer from "../assets/summer.png";
+
+const Characters = [
+    // {
+    //     id: uuidv4(),
+    //     name: 'Beth',
+    //     src: beth,
+    //     isClicked: false
+    // },
+    {
+        id: uuidv4(),
+        name: 'Bird Person',
+        src: birdperson,
+        isClicked: false
+    },
+    {
+        id: uuidv4(),
+        name: 'Jerry',
+        src: jerry,
+        isClicked: false
+    },
+    {
+        id: uuidv4(),
+        name: 'Mee6',
+        src: mee6,
+        isClicked: false
+    },
+    {
+        id: uuidv4(),
+        name: 'Morty',
+        src: morty,
+        isClicked: false
+    },
+    {
+        id: uuidv4(),
+        name: 'Pickle Rick',
+        src: picklerick,
+        isClicked: false
+    },
+    {
+        id: uuidv4(),
+        name: 'Rick',
+        src: rick,
+        isClicked: false
+    },
+    {
+        id: uuidv4(),
+        name: 'Snuffles',
+        src: snuffles,
+        isClicked: false
+    },
+    {
+        id: uuidv4(),
+        name: 'Summer',
+        src: summer,
+        isClicked: false
+    },
+]
+
+export default Characters;
