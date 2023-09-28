03ef7273c633005304eecf3789db8c0e75c14c2c
heyitslauuu <laurencecadag1@gmail.com>
2023-09-28 21:26:47 +0800

    Card Hover State Styling

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -2,11 +2,15 @@ import PropTypes from 'prop-types';
 
 function Card({cardCharacters, cardClick}){
 	return (
-			<div className='grid grid-flow-row-dense grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-6 px-6'>
+			<div className=' grid grid-flow-row-dense grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 py-6 px-6 '>
 					{
             cardCharacters.map((char) => {
               return (
-                <div key={char.id} className='px-4 py-4 rounded-lg' style={{ backgroundColor: 'rgba(200, 100, 150, 0.4)'}} onClick={cardClick}>
+                <div key={char.id} 
+									className='cursor-pointer px-4 py-4 rounded-lg transition ease-out delay-100 hover:shadow-md hover:shadow-yellow-100' 
+									style={{ backgroundColor: 'rgba(200, 100, 150, 0.4)'}} 
+									onClick={() => cardClick(char)}
+								>
                   <div className='flex items-center justify-center'>
                     <img src={char.src} alt={char.name} className='h-[200px] '/>
                   </div>
